e50d10f0aedcc4478ce836b8abcbd6a12b4fd84f
Iskander <i.shushayev@mycar.kz>
2024-12-17 17:47:19 +0500

    hotfix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { qwikVite } from "@builder.io/qwik/optimizer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [qwikVite()],
+  plugins: [qwikVite({ csr: true })],
   resolve: {
     alias: {
       "@components": "/src/components",
