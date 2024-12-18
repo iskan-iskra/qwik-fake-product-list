7aaf11d8f381e3df6fa975299a154f05afa1880a
Iskander <i.shushayev@mycar.kz>
2024-12-18 11:39:03 +0500

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [qwikVite({ csr: true })],
   resolve: {
